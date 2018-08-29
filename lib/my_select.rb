b2910d9f03049b3c41094f8216b2e0c510d71260
Ignacio R Espino <irespino@capozios.com>
2018-08-29 02:43:22 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+ i = 0
+ select_array = []
+ while i < collection.length do
+   if yield(collection[i])
+     select_array.push(yield(collection[i]))
+     i += 1
+   else
+     i += 1
+   end
+ end
+ select_array
 end
